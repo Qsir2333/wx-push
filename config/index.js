5a0782c8effdb051f6891294a9b188d8a227646f
Qsir2333 <142085062+Qsir2333@users.noreply.github.com>
2023-08-12 16:50:52 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx34a1230fd737df37",
+  appsecret: "5c9bca4cf60704d19b98a68781604a53",
+  template_id: "J8CsifDPL53gIwPaePRrYrBFviOuyKVaudNrNz2yZ5U",
+  touser: "ononQ6m9OEU95qnggzktWzNvXk9M",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "上海",
+  first_date: "2019-08-17",
 };
